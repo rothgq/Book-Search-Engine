117dd1cea450632dfc62e07ac6926ec4aa82a9e2
rothgq <rothgq@gmail.com>
2024-03-09 20:27:28 -0500

    typo

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const db = require('./config/connection');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { authMiddleware } = require('./utils/auth');
-const { typeDefs, resolvers } = reqire('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
